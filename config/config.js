cffc338be9986c95bf272a95737842105b40734b
jbroglio <jbroglio@gmail.com>
2017-02-25 15:33:33 -0500

    client and server first commit

---
+++ config/config.js
@@ -0,0 +1,99 @@
+/** config.js
+ * Copyright 2017 Zato, Inc.
+ *
+ * Author: jbroglio
+ * Date: 2/24/17
+ * Time: 7:58 AM
+ */
+var path = require('path'),
+    certPath = path.resolve(__dirname, 'cert'),
+    fs = require('fs'),
+    util = require('util'),
+    bunyan=require('bunyan'),
+    log = bunyan.createLogger({name: 'config'}),
+    extend = require('node.extend'),
+    crypto = require('crypto'),
+    cred = require('./credentials')
+    ;
+
+// overrides is another config with just the changes filled in.
+
+var config =
+{
+  site: '',
+  certs: {
+    clientCert: path.resolve(certPath, 'client.pem'),
+    clientKey: path.resolve(certPath, 'client.key'),
+    serverKey: path.resolve(certPath, 'server.key'),
+    serverCert: path.resolve(certPath, 'server.pem'),
+    caCert: path.resolve(certPath, 'ca.pem')
+  },
+  restart: {
+    timeout: 15000,
+    limit: 10
+  },
+  binDir: '/home/zato/bin',
+  log: {
+    file: 'client.log',
+    level: 'INFO'
+  },
+  ssl: false,
+  port: 3000,
+  mongodb: {
+    urlskel: 'mongodb://'+cred+'localhost:27017/%s?reconnect=true&authSource=admin',
+    clientdb: 'pophealth-production',
+    serverdb: 'notarization',
+    logs: 'logs',
+    nota: 'nota',
+    since: new Date("2015-01-01")
+  },
+  providers: [
+    'localhost:3000'
+  ],
+  serverCert: '',
+  serverKey: '',
+  caCert: '',
+  notarizer:{
+    host: "localhost",
+    port: 3000,
+    app: "/notar"
+  },
+  https: {
+    key: '',
+    cert: '',
+    ca: '',
+    requestCert: true,
+    rejectUnauthorized: false
+  },
+  routes: {
+  },
+  requestOpts:{
+    key:null,
+    cert:null,
+    ca:null,
+    passphrase:''
+  },
+  sha2: function(txt){
+    return crypto.createHash('sha224').update(txt).digest('hex');
+  }
+};
+
+function init(overrides) {
+  config = extend(true, config, overrides);
+  config.clientCert = fs.readFileSync(config.certs.clientCert);
+  config.clientKey = fs.readFileSync(config.certs.clientKey);
+  config.serverCert = fs.readFileSync(config.certs.serverCert);
+  config.serverKey = fs.readFileSync(config.certs.serverKey);
+
+  config.caCert = fs.readFileSync(config.certs.caCert);
+  // config.https.key = config.serverKey;
+  // config.https.cert = config.serverCert;
+  // config.https.ca = config.caCert;
+  config.requestOpts.cert = config.clientCert;
+  config.requestOpts.key = config.clientKey;
+  config.requestOpts.ca = config.caCert;
+
+  return config;
+}
+
+exports.init = init;
---
+++ routes/notar-routes.js
@@ -0,0 +1,124 @@
+// notar-routes.js
+var path = require('path'),
+    app = require(path.resolve(__dirname, '../app')),
+    bunyan = require('bunyan'),
+    log = bunyan.createLogger({name:'notar-routes', "level": "info"}),
+    mongoskin = require('mongoskin'),
+    extend=require('node.extend'),
+    util = require('util'),
+    configPath = path.resolve(__dirname, '../config/'),
+    http = require('http'),
+    https = require('https'),
+    argv=require('yargs')
+        .usage("node notar-routes.js [ --ssl ] [--level LOGLEVEL] [--serverdb SERVER_DATABASE")
+        .boolean('ssl')
+        .argv
+    ;
+
+
+var calls = {
+  help: "/notar/help",
+  sign: "POST /notar/sign {hash: <hash>}",
+  validate: "POST /notar/validate {hash: <hash>, signature: <key>, time:<timestamp>",
+  NOTE: "9180 requires authentication"
+};
+var coll; // db collection
+
+function init() {
+
+  app.get('/notar/help', function (req, res, next) {
+    res.send(JSON.stringify(util.format(calls)));
+  })
+
+
+  app.post('/notar/sign', function (req, res, next) {
+    var hash = req.body.hash;
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    var id = req.body.id
+    if (!hash) return res.status(400).send("/notar/sign: No hash element in post body");
+    try {
+      coll.insertOne({ip: ip, itemid: id, hash: hash},// time: new Date(body.timestamp)},
+          function (err, wresult) {
+            if (err) {
+              log.error(err, "signing hash from %s", ip);
+              return res.status(500).send("/notar/sign: Error inserting hash");
+            }
+            res.send(JSON.stringify({signature: wresult.insertedId.toString()}));
+          })
+    } catch (e){
+      log.error(e, "writing db");
+    }
+  });
+
+  app.post('/notar/validate', function (req, res, next) {
+    var hash = req.body.hash,
+        signature = req.body.signature,
+        timestamp = new Date(req.body.time),
+        id = req.body.id
+        ;
+    coll.findOne({_id: mongoskin.ObjectId(signature)}, function (err, ob) {
+      if (err) {
+        log.error(err, "/notar/validate from ip:%s for signature:%s", ip, signature);
+        return res.status(500).send("error finding signature")
+      }
+      if (!ob) {
+        log.error("No object for signature:%s", signature);
+        return res.send({invalid: true, reason: "no such signature"});
+      }
+      if (ob.hash == hash && ob.itemid == id) return res.send('OK');
+      //else
+      coll.update({_id: mongoskin.ObjectId(signature)}, {'$set': {invalid: true}});
+      return res.send(JSON.stringify(
+          {invalid: true, reason: (ob.hash != hash ? "hash " : "id ") + "does not match"}))
+    })
+  });
+
+  app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
+
+}
+//================  MAIN ======================
+if (require.main == module) {
+  var configFile = argv.config || path.resolve(configPath, 'config.js');
+  config = require(configFile).init();
+  if (argv.level) log.level(argv.level);
+  if (argv.serverdb) config.mongodb.serverdb = argv.serverdb;
+  if (argv.ssl) config.ssl=true;
+  options = extend({}, config.requestOpts);
+  options.url = config.notarizer.url;
+  options.json = true;
+  options.jar = true;
+
+  var db;
+  try {
+    db = mongoskin.MongoClient.connect(
+        util.format(config.mongodb.urlskel, config.mongodb.serverdb),
+        {native_parser: true});
+  }
+  catch (err) {
+    dbConnectFailure(err);
+  }
+
+  coll = db.collection(config.mongodb.nota);
+
+  function dbConnectFailure(err)
+  {
+    log.error('Unable to connect to mongo at %s.\n   ', config.mongodb.urlskel, err.message);
+    setTimeout(function(){process.exit(1);}, 3000);
+  }
+
+  init();
+  startServer();
+  var server;
+
+  function startServer() {
+    if (config.ssl) server = https.createServer(config.https, app);
+    else server = http.createServer(app);
+    server.listen(config.port);
+    console.log(new Date() + ": " + (config.ssl ? "HTTPS" : "HTTP") + " Listening at " + config.port);
+  }
+
+}
